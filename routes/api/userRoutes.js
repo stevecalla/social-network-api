2963d13795a3a5038d749e7ed544d8d37476b445
Steve Calla <callasteven@gmail.com>
2022-10-16 22:33:21 -0600

    Add delete user route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,12 +3,13 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  deleteUser
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
 
 module.exports = router;
